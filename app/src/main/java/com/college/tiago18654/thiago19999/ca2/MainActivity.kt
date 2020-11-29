72ab557d62df5405911da375e5ed50d74cb17082
Thiagodiass <36368115+Thiagodiass@users.noreply.github.com>
2020-11-29 11:16:13 +0000

    Button Normalised Initial screen Just missing Menu Page

--- app/src/main/java/com/college/tiago18654/thiago19999/ca2/MainActivity.kt
+++ app/src/main/java/com/college/tiago18654/thiago19999/ca2/MainActivity.kt
@@ -22,11 +22,12 @@ import androidx.appcompat.app.AppCompatActivity
 /**
  * Creates an Activity that hosts all of the fragments in the app
  */
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.inicial_fragment)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
     }
 
 }
